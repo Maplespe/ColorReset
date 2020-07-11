dbbc6ba99c337f203749e6b26b8688cb5cec7f63
Maplespe <43676879+Maplespe@users.noreply.github.com>
2020-07-11 21:49:46 +0800

    Add files via upload

---
+++ ColorReset/ColorReset.cpp
@@ -0,0 +1,202 @@
+﻿// ColorReset.cpp : 定义应用程序的入口点。
+//
+
+#include "framework.h"
+#include "ColorReset.h"
+#include "easyhook.h"
+#include <shellapi.h>
+#include <Shlwapi.h>
+#include <string>
+
+#if _M_X64
+#pragma comment(lib,"EasyHook64.lib")
+#else
+#pragma comment(lib,"EasyHook32.lib")
+#endif
+
+#pragma comment(lib, "shlwapi.lib")
+
+#pragma region API
+
+//文件是否存在
+bool FileIsExist(std::wstring FilePath) {
+	WIN32_FIND_DATA  FindFileData;
+	HANDLE hFind;
+	hFind = FindFirstFile(FilePath.c_str(), &FindFileData);
+	if (hFind != INVALID_HANDLE_VALUE) {
+		FindClose(hFind);
+		return true;
+	}
+	return false;
+}
+
+//字符串子文本替换
+std::wstring replace_all(std::wstring str, std::wstring old_value, std::wstring new_value)
+{
+	while (true) {
+		std::wstring::size_type   pos(0);
+		if ((pos = str.find(old_value)) != std::wstring::npos)
+			str.replace(pos, old_value.length(), new_value);
+		else   break;
+	}
+	return str;
+}
+
+//取当前目录
+std::wstring GetCurExeDir() {
+	wchar_t sPath[MAX_PATH];
+	GetModuleFileNameW(0, sPath, MAX_PATH);
+	std::wstring path = sPath;
+	path = path.substr(0, path.rfind(L"\\"));
+
+	return path;
+}
+
+//读取注册表文本型
+std::wstring GetRegSZ(HKEY hKey, LPCWSTR SubKey, LPCWSTR KeyName, bool is64 = false) {
+
+	HKEY tkey;
+
+	REGSAM sam = KEY_READ;
+	if (is64)
+		sam = KEY_READ | KEY_WOW64_64KEY;
+
+	if (ERROR_SUCCESS == RegOpenKeyExW(hKey, SubKey, 0, sam, &tkey))
+	{
+		wchar_t dwValue[1024];
+		DWORD dwSzType = REG_SZ;
+		DWORD dwSize = sizeof(dwValue);
+		if (RegQueryValueExW(tkey, KeyName, 0, &dwSzType, (LPBYTE)&dwValue, &dwSize) != ERROR_SUCCESS)
+		{
+			return L"Query Error";
+		}
+		return dwValue;
+	}
+	RegCloseKey(tkey);
+	return L"Open Error";
+}
+
+//取扩展程序路径
+std::wstring GetExtensionExe(std::wstring filePath)
+{
+	std::wstring _Path = GetRegSZ(HKEY_CLASSES_ROOT, PathFindExtensionW(filePath.c_str()), L"");
+	if (_Path != L"" && _Path != L"Open Error" && _Path != L"Query Error")
+	{
+		_Path = GetRegSZ(HKEY_CLASSES_ROOT, (_Path + L"\\shell\\open\\command").c_str(), L"");
+		if (_Path == L"" || _Path == L"Open Error" || _Path == L"Query Error")
+		{
+			_Path = GetRegSZ(HKEY_CLASSES_ROOT, (_Path + L"\\shell\\edit\\command").c_str(), L"");
+		}
+	}
+	if (_Path != L"" && _Path != L"Open Error" && _Path != L"Query Error")
+	{
+		LPWSTR* szArgList;
+		int argCount;
+
+		szArgList = CommandLineToArgvW(_Path.c_str(), &argCount);
+		if (szArgList != NULL)
+		{
+			_Path = szArgList[0];
+			LocalFree(szArgList);
+			return _Path;
+		}
+	}
+	return L"";
+}
+
+#pragma endregion
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR   lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+
+	LPWSTR* szArgList;
+	int argCount;
+
+	szArgList = CommandLineToArgvW(lpCmdLine, &argCount);
+	if (szArgList == NULL)
+	{
+		return 10;
+	}
+
+	//取自身路径
+	wchar_t sPath[MAX_PATH];
+	GetModuleFileNameW(0, sPath, MAX_PATH);
+	std::wstring curFileName = sPath;
+
+	for (int i = 0; i < argCount; i++)
+	{
+		std::wstring curcmd = szArgList[i];
+		//确保不是自己
+		if (curcmd != curFileName)
+		{
+			//如果不是exe 那就找关联程序
+			std::wstring cmd_ = L"";
+
+			std::wstring extstr = PathFindExtensionW(curcmd.c_str());
+			if (replace_all(extstr, L".EXE", L".exe") != L".exe") {
+				//将原参数保存
+				cmd_ = curcmd;
+				//取此扩展名所关联的应用程序
+				curcmd = GetExtensionExe(curcmd);
+			}
+
+			//确定文件存在且是exe
+			if (FileIsExist(curcmd) && replace_all(curcmd, L".EXE", L".exe").rfind(L".exe") != -1)
+			{
+				//初始化结构体
+				STARTUPINFO stStartUpInfo;
+				memset(&stStartUpInfo, 0, sizeof(stStartUpInfo));
+				stStartUpInfo.cb = sizeof(stStartUpInfo);
+
+				PROCESS_INFORMATION stProcessInfo;
+				memset(&stProcessInfo, 0, sizeof(stProcessInfo));
+
+				wchar_t* _cmd_ = NULL;
+
+				//拷贝源参数
+				if (cmd_ != L"") {
+					cmd_ = L" \"" + cmd_ + L"\"";
+					//拷贝字符串 CreateProcessW 的参数不可为const
+					_cmd_ = new wchar_t[cmd_.length() + 1];
+					ZeroMemory(_cmd_, cmd_.length());
+					//从源字符串复制数据到内存
+					memcpy(_cmd_, cmd_.data(), cmd_.length() * sizeof(wchar_t));
+					//添加字符串结尾
+					_cmd_[cmd_.length()] = '\0';
+				}
+
+				//创建进程
+				bool ret = CreateProcessW(curcmd.c_str(), _cmd_, NULL, NULL, false, CREATE_NEW_CONSOLE, NULL, NULL, &stStartUpInfo, &stProcessInfo);
+
+				//释放
+				if (_cmd_ != NULL)
+					delete[] _cmd_;
+
+				if (ret)
+				{
+					//注入扩展
+#if _M_X64
+					std::wstring dllPath = GetCurExeDir() + L"\\ColorResetLib.dll";
+					NTSTATUS ntStatus = RhInjectLibrary(stProcessInfo.dwProcessId, 0, EASYHOOK_INJECT_DEFAULT, NULL, (WCHAR*)dllPath.c_str(),  NULL, 0);
+#else
+					std::wstring dllPath = GetCurExeDir() + L"\\ColorResetLib32.dll";
+					NTSTATUS ntStatus = RhInjectLibrary(stProcessInfo.dwProcessId, 0, EASYHOOK_INJECT_DEFAULT, (WCHAR*)dllPath.c_str(), NULL, NULL, 0);
+#endif
+					if (ntStatus != 0)
+					{
+						std::wstring error = RtlGetLastErrorString();
+						MessageBoxW(0, (L"对进程加载ColorResetLib.dll失败!\n错误信息:\n" + error).c_str(), L"失败", MB_ICONERROR);
+					}
+				}
+				else
+					MessageBoxW(0, L"CreateProcess Failed!", L"Error", MB_ICONERROR);
+			}
+		}
+	}
+
+	LocalFree(szArgList);
+    return (int) 0;
+}
---
+++ ColorResetLib/dllmain.cpp
@@ -0,0 +1,135 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "pch.h"
+#include "MinHook.h"
+#include "..\ColorReset\easyhook.h"
+#include <Uxtheme.h>
+#pragma comment(lib, "uxtheme.lib")
+
+HMODULE g_hModule;
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    g_hModule = hModule;
+    return TRUE;
+}
+
+extern DWORD WINAPI MGetSysColor(int nIndex);
+typedef DWORD(WINAPI* O_GetSysColor)(int);
+O_GetSysColor F_GetSysColor = 0;
+
+extern HRESULT WINAPI MGetThemeColor(HTHEME hTheme, int iPartId, int iStateId, int iPropId, COLORREF* pColor);
+typedef HRESULT(WINAPI* O_GetThemeColor)(HTHEME, int, int, int, COLORREF*);
+O_GetThemeColor F_GetThemeColor = 0;
+
+//全局主题句柄
+HTHEME g_hTheme = 0;
+
+//EasyHook EntryPoint
+extern "C" __declspec(dllexport) BOOL CALLBACK NativeInjectionEntryPoint(REMOTE_ENTRY_INFO * pInfo)
+{
+    if (MH_Initialize() == MH_OK)
+    {
+        MH_CreateHook(&GetSysColor, &MGetSysColor, (LPVOID*)&F_GetSysColor);
+        MH_CreateHook(&GetThemeColor, &MGetThemeColor, (LPVOID*)&F_GetThemeColor);
+
+        if (MH_EnableHook(&GetSysColor) == MH_OK
+            && MH_EnableHook(&GetThemeColor) == MH_OK)
+        {
+            
+            MessageBeep(MB_OK);
+            //MessageBoxW(0, L"内鬼成功安插a.a(你的颜色会变成棕色)", L"2333", MB_ICONINFORMATION);
+        }
+        else
+            MessageBoxW(0, L"函数加载失败..", L"Fuck", MB_ICONERROR);
+    }
+    else
+        MessageBoxW(0, L"初始化反汇编引擎失败!", L"Fuck", MB_ICONERROR);
+
+    return true;
+}
+
+DWORD WINAPI MGetSysColor(int nIndex)
+{
+    DWORD Color = 0;
+    //将所有索引值的颜色 都设置为指定颜色(以下颜色值是系统默认主题颜色)
+    if (nIndex == COLOR_SCROLLBAR)
+        Color = RGB(200, 200, 200);
+    else if (nIndex == COLOR_DESKTOP || nIndex == COLOR_BACKGROUND)
+        Color = RGB(0, 0, 0);
+    else if (nIndex == COLOR_ACTIVECAPTION)
+        Color = RGB(153, 180, 209);
+    else if (nIndex == COLOR_INACTIVECAPTION)
+        Color = RGB(191, 205, 219);
+    else if (nIndex == COLOR_MENU)
+        Color = RGB(240, 240, 240);
+    else if (nIndex == COLOR_WINDOW)
+        Color = RGB(255, 255, 255);
+    else if (nIndex == COLOR_WINDOWFRAME)
+        Color = RGB(100, 100, 100);
+    else if (nIndex == COLOR_MENUTEXT)
+        Color = RGB(0, 0, 0);
+    else if (nIndex == COLOR_WINDOWTEXT)
+        Color = RGB(0, 0, 0);
+    else if (nIndex == COLOR_CAPTIONTEXT)
+        Color = RGB(0, 0, 0);
+    else if (nIndex == COLOR_ACTIVEBORDER)
+        Color = RGB(180, 180, 180);
+    else if (nIndex == COLOR_INACTIVEBORDER)
+        Color = RGB(244, 247, 252);
+    else if (nIndex == COLOR_APPWORKSPACE)
+        Color = RGB(171, 171, 171);
+    else if (nIndex == COLOR_HIGHLIGHT)
+        Color = RGB(0, 120, 215);
+    else if (nIndex == COLOR_HIGHLIGHTTEXT)
+        Color = RGB(255, 255, 255);
+    else if (nIndex == COLOR_3DFACE)
+        Color = RGB(240, 240, 240);
+    else if (nIndex == COLOR_3DSHADOW || nIndex == COLOR_BTNSHADOW)
+        Color = RGB(160, 160, 160);
+    else if (nIndex == COLOR_GRAYTEXT)
+        Color = RGB(109, 109, 109);
+    else if (nIndex == COLOR_BTNTEXT)
+        Color = RGB(0, 0, 0);
+    else if (nIndex == COLOR_INACTIVECAPTIONTEXT)
+        Color = RGB(0, 0, 0);
+    else if (nIndex == COLOR_BTNHIGHLIGHT || nIndex == COLOR_BTNHILIGHT
+        || nIndex == COLOR_3DHILIGHT || nIndex == COLOR_3DHIGHLIGHT)
+        Color = RGB(255, 255, 255);
+    else if (nIndex == COLOR_3DDKSHADOW)
+        Color = RGB(105, 105, 105);
+    else if (nIndex == COLOR_3DLIGHT)
+        Color = RGB(227, 227, 227);
+    else if (nIndex == COLOR_INFOTEXT)
+        Color = RGB(0, 0, 0);
+    else if (nIndex == COLOR_INFOBK)
+        Color = RGB(255, 255, 225);
+    else if (nIndex == COLOR_HOTLIGHT)
+        Color = RGB(0, 102, 204);
+    else if (nIndex == COLOR_GRADIENTACTIVECAPTION)
+        Color = RGB(185, 209, 234);
+    else if (nIndex == COLOR_GRADIENTINACTIVECAPTION)
+        Color = RGB(215, 228, 242);
+    else if (nIndex == COLOR_MENUHILIGHT)
+        Color = RGB(0, 120, 215);
+    else if (nIndex == COLOR_MENUBAR)
+        Color = RGB(240, 240, 240);
+    else
+        Color = 0;
+
+    return Color;
+}
+
+HRESULT __stdcall MGetThemeColor(HTHEME hTheme, int iPartId, int iStateId, int iPropId, COLORREF* pColor)
+{
+    /*
+    if(pColor)
+        *pColor = RGB(87, 80, 70);*/
+
+    //主题颜色太多了
+    //暂时还没需求... 需要的自己判断添加
+
+    return F_GetThemeColor(hTheme, iPartId, iStateId, iPropId, pColor);
+}
